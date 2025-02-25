bcb6fd7d1383284f2f2c6b5666478f72b29cf45f
KonyD <asilpoyrazsahin@gmail.com>
2025-02-25 21:30:23 +0300

    Changed the title

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Pyrogue: A roguelike made in python",
         vsync=True,
     ) as context:
         root_console = tcod.console.Console(screen_width, screen_height, order="F")
